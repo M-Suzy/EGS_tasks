d8ea61319489950de5189655b8804721a7d634cd
Suzie Melkonyan <melkonyansuzie@gmail.com>
2020-03-19 20:38:11 +0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       new  UserDataHandler();
+      /*  File hello = new File("hello.txt");
+        File result = new File("result.txt");
+        if(result.exists()){
+            result.delete();
+            result.createNewFile();
+        }
+        hello.createNewFile();
+        FileHandler.CountHellos(hello, result);*/
+    }
+}
